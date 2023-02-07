71b80964d736e1f1a112b5bb9bff91e58b407ba1
Yiwyn <5597104@qq.com>
2023-02-07 22:07:38 +0800

    项目模块添加

---
+++ lf-file-fileManager/src/main/java/fun/lifepoem/manager/FileManagerApplication.java
@@ -0,0 +1,15 @@
+package fun.lifepoem.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Yiwyn
+ * @create 2023/2/7 22:00
+ */
+@SpringBootApplication
+public class FileManagerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FileManagerApplication.class, args);
+    }
+}
