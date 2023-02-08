c5111ca8ff677d99b3e4e9d59bb0dd809319d5c0
Yiwyn <5597104@qq.com>
2023-02-08 23:09:35 +0800

    init

--- lf-file-fileManager/src/main/java/fun/lifepoem/manager/utils/FileUploadUtils.java
+++ lf-file-fileManager/src/main/java/fun/lifepoem/manager/utils/FileUploadUtils.java
@@ -7,6 +7,11 @@ package fun.lifepoem.manager.utils;
 public class FileUploadUtils {
 
     private static final long FILE_MAX_SIZE = 50 * 1024 * 1024;
+    private static final long FILE_NAME_MAX_SIZE = 128;
 
+
+    public static String upload() {
+        return null;
+    }
 
 }
