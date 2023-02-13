b328c92ccd72641bb9a421e5098f10039c4af078
Yiwyn <5597104@qq.com>
2023-02-13 21:48:59 +0800

    封装分享组件

---
+++ lf-file-fmanager/src/main/java/fun/lifepoem/manager/domain/LpShareRecord.java
@@ -0,0 +1,49 @@
+package fun.lifepoem.manager.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+import lombok.Data;
+
+/**
+ * 
+ * @TableName lp_share_record
+ */
+@Data
+public class LpShareRecord implements Serializable {
+    /**
+     * 
+     */
+    private Integer id;
+
+    /**
+     * 分享人id
+     */
+    private Integer userId;
+
+    /**
+     * 分享码
+     */
+    private String shareKey;
+
+    /**
+     * 分享链接
+     */
+    private String shareLink;
+
+    /**
+     * 过期时间
+     */
+    private Date expiryDt;
+
+    /**
+     * 过期状态
+     */
+    private Boolean expiryStatus;
+
+    /**
+     * 分享时间
+     */
+    private Date createDt;
+
+    private static final long serialVersionUID = 1L;
+}
---
+++ lf-file-fmanager/src/main/java/fun/lifepoem/manager/mapper/LpShareRecordMapper.java
@@ -0,0 +1,25 @@
+package fun.lifepoem.manager.mapper;
+
+import fun.lifepoem.manager.domain.LpShareRecord;
+
+/**
+* @author 55971
+* @description 针对表【lp_share_record】的数据库操作Mapper
+* @createDate 2023-02-13 21:46:42
+* @Entity fun.lifepoem.manager.domain.LpShareRecord
+*/
+public interface LpShareRecordMapper {
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(LpShareRecord record);
+
+    int insertSelective(LpShareRecord record);
+
+    LpShareRecord selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(LpShareRecord record);
+
+    int updateByPrimaryKey(LpShareRecord record);
+
+}
